77b6f138af407aca2cba7c2ca30838e0d0ca0b46
Jorge <jorgeauri@gmail.com>
2021-09-12 22:13:43 -0300

    fourth commit

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,8 +1,29 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 export const Container = styled.aside`
   background-color: ${(props) => props.theme.colors.background};
   widht: 360px;
   height: 100vh;
   overflow-y: auto;
+`;
+
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  backgound-color: #ffffff;
+  padding: 16px;
+`;
+
+export const Logo = styled.img`
+  margin-bottom: 15px;
+`;
+
+export const Map = styled.div`
+  background-color: red;
+  width: 500px;
 `;
